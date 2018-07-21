8bd21263910e4adf38df420d8dc4da3563973ee1
LuisRonquillo <lronquillo977@gmail.com>
2018-07-20 21:18:34 -0500

    conexión a servidor (solicitar IP a diosemir)
    Conexión a BD en servidor. diosemir debe dar de alta sus IP para testear.

--- ProyectoLibros/Models/conexion.js
+++ ProyectoLibros/Models/conexion.js
@@ -11,13 +11,21 @@ var connection = mysql.createConnection({
 });*/
 
 /**conexion 2 */
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host:"localhost",
     port:3306,
     user:"root",
     password:"kjkszpj08",
     database:"bdlibros",
     insecureAuth:true
+});*/
+//conexión al servidor
+var connection = mysql.createConnection({host: "bookstorebd.mysql.database.azure.com", 
+	user: "DiosemirNah@bookstorebd.com", 
+	password: "Admin12345", 
+	database: "bookstore", 
+	port: 3306, 
+	ssl:true
 });
 
 connection.connect();
