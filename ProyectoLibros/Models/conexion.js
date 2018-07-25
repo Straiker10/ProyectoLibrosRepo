92225f4cb887cb2111c764ef89b4baaee0cbbae6
Carlos Estrella <ae081195@gmail.com>
2018-07-25 11:53:25 -0500

    solo conexion servidor

--- ProyectoLibros/Models/conexion.js
+++ ProyectoLibros/Models/conexion.js
@@ -1,42 +1,5 @@
 var mysql = require('mysql');
 
-/**var connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user:"root",
-    password:"Ronquillo#2",
-    database:"bdlibros",
-    insecureAuth:true
-});*/
-///conexion 1 
-
-<<<<<<< HEAD
-/*
-var connection = mysql.createConnection({
-=======
-/*var connection = mysql.createConnection({
->>>>>>> a75889d2a72988e7a8890aa9db07b40b5db392fe
-    host:"localhost",
-    port:3306,
-    user:"root",
-    password:"",
-    database:"bdlibros",
-    insecureAuth:true
-});*/
-<<<<<<< HEAD
-
-=======
->>>>>>> a75889d2a72988e7a8890aa9db07b40b5db392fe
-/**conexion 2 */
-var connection = mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    user:"root",
-    password:"kjkszpj08",
-    database:"bdlibros",
-    insecureAuth:true
-});
-
 //conexión al servidor
 var connection = mysql.createConnection({host: "bookstorebd.mysql.database.azure.com", 
 	user: "DiosemirNah@bookstorebd.com", 
